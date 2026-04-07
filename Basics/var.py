6300bbbc8ea7204fc44294f749de8dbba18ae30a
Zahid Khan <zahidkhan18100@gmail.com>
2026-04-07 22:58:54 +0530

    Implement variable initialization and type examples
    Demonstrate variable initialization with different types including int, str, complex, bool, and None.

---
+++ Basics/var.py
@@ -0,0 +1,19 @@
+x = 10
+x = "ZAHID"
+print(x)
+"""age = 18 """
+a = complex(6 , 2)
+b = True
+c = "KHAN"
+d = None 
+print(a)
+print(b)
+a1 = 9
+print(a +a1)
+print(type(a))
+print(type(b))
+print( "THE TYPE OF c",type(c))
+# start with number is not valid 
+# hyphen is not alloed
+# \n is use for new line 
+# data types are float , int , bool , str
