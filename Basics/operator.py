abe97127eb5f9a2f821225b7b82b63954199c814
Zahid Khan <zahidkhan18100@gmail.com>
2026-04-09 19:44:39 +0530

    Add basic  operations in operator.py
    Implemented basic arithmetic operations including addition, subtraction, multiplication, division, modulus, exponentiation, and floor division with print statements for output.

---
+++ Basics/operator.py
@@ -0,0 +1,23 @@
+# let us see an example of operator
+# there are two known variables n and m
+ 
+n = 5
+m = 8
+ans1 = n+m
+print("addition of n and m is " , ans1 ) # BASIC ADDITION OPERATOR 
+ans2 = n-m 
+print("subtraction of n and m is ", ans2) # BASIC SUBTRACTION OPERATOR 
+ans3 = n*m
+print("multiplication of n and m is" , ans3) # BASIC MULTIPLICATION OPERATOR
+ans4 = n/m
+print("Division of n and m is" , ans4) # Division operator (/) — returns float
+ans5 = n%m
+print("modulus of n and m is" , ans5)  # Modulus operator (%) — returns remainder
+ans6 = n**m
+print("the exponential of n and m is" , ans6) # Exponentiation operator (**) — raises n to the power of m
+ans7 = n//m
+print(" floor division is " , ans7) # Floor division operator (//) — returns quotient without decimal
+
+
+
+
