70ba7c9ff8ff418cf9c06c6e77e117b7882062c0
Zahid Khan <zahidkhan18100@gmail.com>
2026-05-11 22:00:59 +0530

    Enhance string method examples in 08 stringmethod.py
    Added examples for string methods including capitalize, center, count, and endswith.

--- Basics/08 stringmethod.py
+++ Basics/08 stringmethod.py
@@ -8,3 +8,13 @@ print(a.lower())  # lower method in string
 print(a.rstrip("!")) # rstrip method in string( Removes trailing characters from the right side of the string. and removes specified characters from the end of the string. If no argument is passed, whitespace is removed.
 print(a.replace("Khan", "Zahid")) # it will replace string 1 to another.
 print(a.split(" ")) # Splits a string into a list.
+
+text = "intorduction to python"
+print(text.capitalize()) # capitalize() returns a string with the first letter capitalized and all other letters lowercase
+
+print(a.center(70)) # center(width) returns a centered string of specified width by adding spaces on both sides.
+print(a.count("Khan")) #count(value) returns the total occurrences of the specified substring.
+
+text = "Hello.py"
+print(text.endswith(".py")) # Returns True if the string ends with the specified suffix, otherwise False.
+
